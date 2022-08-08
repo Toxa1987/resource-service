1a19e5d05a61a32c75d56d922f37d5ae4c0ab11f
Anton Pysk <apysk@goddady.com>
2022-08-08 15:03:57 +0300

    update docker-compose.yml, application.yml

--- src/main/java/com/epam/esm/resourceservice/ResourceServiceApplication.java
+++ src/main/java/com/epam/esm/resourceservice/ResourceServiceApplication.java
@@ -2,10 +2,12 @@ package com.epam.esm.resourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableRetry
+@EnableEurekaClient
 public class ResourceServiceApplication {
 
     public static void main(String[] args) {
